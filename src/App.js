d8f33a5e28c64f630e68750fe120ceefea8b9b0f
Kenneth <kenneth@creative-mischief.com>
2017-11-24 21:29:22 -0500

    ready for coding react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+export default class App extends Component {
+
+  // constructor (props) {
+    // super(props);
+    // this.state = {
+    //   id: 0
+    // }
+    // this.setDetail = this.setDetail.bind(this);
+  // }
+
+  render() {
+    return (
+    	<div> Hello World </div>
+    );
+  }
+}
