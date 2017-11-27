578eee222c58a6d8df9c8d9d59e28b183a5e3afd
Kenneth <kenneth@creative-mischief.com>
2017-11-27 17:04:31 -0500

    json structure

---
+++ src/Info.js
@@ -0,0 +1,246 @@
+[
+	{
+		name:'Model S',
+		home: {
+			thumb: 'Car Thumbnail',
+			tagline: 'text under name',
+		}
+		page: {
+			tagline: 'text for specific car',
+			summary: 'text under hero',
+			hero: 'hero image',
+			threeD: '3d image extension',
+			modules: [
+				{
+					image: 'image for module',
+					text: 'text for module',
+				},
+				{
+					image: 'image for module',
+					text: 'text for module',
+				},
+				{
+					image: 'image for module',
+					text: 'text for module',
+				},
+				{
+					image: 'image for module',
+					text: 'text for module',
+				},
+			],
+			specs: {
+				specsHero: 'img link',
+				performance: {
+					maxSpeed: '328KPH (204MPH)',
+					zero100:'3.4s',
+					zero200:'9.8s',
+					maxPwr:'570PS (562BHP)',
+					maxTor:'600NM (443LBFT)',
+				},
+				engine: {
+					engineCapacity:'3,799CC',
+					type:'V8',
+					tech:'Twin-Turbo',
+				},
+				braking: {
+					twoTo0: '133m (436FT)',
+					oneTo0:'33M (108FT)'
+				},
+				efficiency: {
+					co2: '249G/KM',
+					pwrToWeight:'434PS (428 BHP)'
+				},
+				weight: {
+					dinWeight:'1,495KG (3,296LB)',
+					weightDistribution:'(F/R) 42%/58%',
+					dryWeight: '1,400KG (1,350KG)'
+				}
+			},
+
+
+		}
+	},
+	{
+		name:'Model S',
+		home: {
+			thumb: 'Car Thumbnail',
+			tagline: 'text under name',
+		}
+		page: {
+			tagline: 'text for specific car',
+			summary: 'text under hero',
+			hero: 'hero image',
+			threeD: '3d image extension',
+			modules: [
+				{
+					image: 'image for module',
+					text: 'text for module',
+				},
+				{
+					image: 'image for module',
+					text: 'text for module',
+				},
+				{
+					image: 'image for module',
+					text: 'text for module',
+				},
+				{
+					image: 'image for module',
+					text: 'text for module',
+				},
+			],
+			specs: {
+				specsHero: 'img link',
+				performance: {
+					maxSpeed: '328KPH (204MPH)',
+					zero100:'3.4s',
+					zero200:'9.8s',
+					maxPwr:'570PS (562BHP)',
+					maxTor:'600NM (443LBFT)',
+				},
+				engine: {
+					engineCapacity:'3,799CC',
+					type:'V8',
+					tech:'Twin-Turbo',
+				},
+				braking: {
+					twoTo0: '133m (436FT)',
+					oneTo0:'33M (108FT)'
+				},
+				efficiency: {
+					co2: '249G/KM',
+					pwrToWeight:'434PS (428 BHP)'
+				},
+				weight: {
+					dinWeight:'1,495KG (3,296LB)',
+					weightDistribution:'(F/R) 42%/58%',
+					dryWeight: '1,400KG (1,350KG)'
+				}
+			},
+
+
+		}
+	},
+	{
+		name:'Model S',
+		home: {
+			thumb: 'Car Thumbnail',
+			tagline: 'text under name',
+		}
+		page: {
+			tagline: 'text for specific car',
+			summary: 'text under hero',
+			hero: 'hero image',
+			threeD: '3d image extension',
+			modules: [
+				{
+					image: 'image for module',
+					text: 'text for module',
+				},
+				{
+					image: 'image for module',
+					text: 'text for module',
+				},
+				{
+					image: 'image for module',
+					text: 'text for module',
+				},
+				{
+					image: 'image for module',
+					text: 'text for module',
+				},
+			],
+			specs: {
+				specsHero: 'img link',
+				performance: {
+					maxSpeed: '328KPH (204MPH)',
+					zero100:'3.4s',
+					zero200:'9.8s',
+					maxPwr:'570PS (562BHP)',
+					maxTor:'600NM (443LBFT)',
+				},
+				engine: {
+					engineCapacity:'3,799CC',
+					type:'V8',
+					tech:'Twin-Turbo',
+				},
+				braking: {
+					twoTo0: '133m (436FT)',
+					oneTo0:'33M (108FT)'
+				},
+				efficiency: {
+					co2: '249G/KM',
+					pwrToWeight:'434PS (428 BHP)'
+				},
+				weight: {
+					dinWeight:'1,495KG (3,296LB)',
+					weightDistribution:'(F/R) 42%/58%',
+					dryWeight: '1,400KG (1,350KG)'
+				}
+			},
+
+
+		}
+	},
+	{
+		name:'Model S',
+		home: {
+			thumb: 'Car Thumbnail',
+			tagline: 'text under name',
+		}
+		page: {
+			tagline: 'text for specific car',
+			summary: 'text under hero',
+			hero: 'hero image',
+			threeD: '3d image extension',
+			modules: [
+				{
+					image: 'image for module',
+					text: 'text for module',
+				},
+				{
+					image: 'image for module',
+					text: 'text for module',
+				},
+				{
+					image: 'image for module',
+					text: 'text for module',
+				},
+				{
+					image: 'image for module',
+					text: 'text for module',
+				},
+			],
+			specs: {
+				specsHero: 'img link',
+				performance: {
+					maxSpeed: '328KPH (204MPH)',
+					zero100:'3.4s',
+					zero200:'9.8s',
+					maxPwr:'570PS (562BHP)',
+					maxTor:'600NM (443LBFT)',
+				},
+				engine: {
+					engineCapacity:'3,799CC',
+					type:'V8',
+					tech:'Twin-Turbo',
+				},
+				braking: {
+					twoTo0: '133m (436FT)',
+					oneTo0:'33M (108FT)'
+				},
+				efficiency: {
+					co2: '249G/KM',
+					pwrToWeight:'434PS (428 BHP)'
+				},
+				weight: {
+					dinWeight:'1,495KG (3,296LB)',
+					weightDistribution:'(F/R) 42%/58%',
+					dryWeight: '1,400KG (1,350KG)'
+				}
+			},
+
+
+		}
+	},
+]
