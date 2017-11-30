18b25a1bb56adb13eb3675ede22ead9429ba0866
Kenneth <kenneth@creative-mischief.com>
2017-11-29 19:36:45 -0500

    counter

--- src/SpecCounter.js
+++ src/SpecCounter.js
@@ -1,37 +1,66 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import styled from 'styled-components';
+import AnimatedNumber from 'react-animated-number';
 
-const Num = styled.h1 `
-  color: red;
+const NumWrap = styled.h1 `
+  > .number {
+  	color: red;
+  }
 
 `
 export default class SpecCounter extends Component {
-	  constructor (props) {
-	    super(props);
-	    this.state = {
-	    	speed:0,
-	    	tor:0,
-	    	zero60:0,
-	    },
-	  }
+	constructor (props) {
+		super(props);
+		this.speed = 0;
+		this.tor = 0;
+		this.zero60 = 0;
+	}
+	componentdidMount() {
+		while(this.speed < this.props.specs.speed) {
+			setTimeout(()=>{this.speed++},100);
+		}
+	}
+
 
 	render() {
 		return(
 			<div>
-				<div>
+				<NumWrap>
 					<h3>Top Speed</h3>
-					<Num>{this.props.specs.speed}</Num>
-				</div>
-				<div>
+					<AnimatedNumber className="number" component="text" value={this.props.specs.speed}
+			            frameStyle={perc => (
+			                perc === 100 ? {} : {}
+			            )}
+			            duration={1100}
+			            stepPrecision={0}
+			        />
+				</NumWrap>
+				<NumWrap>
 					<h3>Torque</h3>
-					<Num>{this.props.specs.tor}</Num>
-				</div>
-				<div>
+					<AnimatedNumber className="number" component="text" value={this.props.specs.tor}
+			            frameStyle={perc => (
+			                perc === 100 ? {} : {}
+			            )}
+			            duration={1000}
+			            stepPrecision={0}
+			        />
+				</NumWrap>
+				<NumWrap>
 					<h3>0 to 60</h3>
-					<Num>{this.props.specs.zero60}</Num>
-				</div>
+					<AnimatedNumber className="number" component="text" value={this.props.specs.zero60}
+			            frameStyle={perc => (
+			                perc === 100 ? {} : {}
+			            )}
+			            duration={1000}
+			            stepPrecision={2}
+			        />
+				</NumWrap>
 			</div>
 		)
 	}
 }
+
+//this.props.specs.speed
+//this.props.specs.tor
+//this.props.specs.zero60
